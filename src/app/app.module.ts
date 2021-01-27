7de7e15d0cc6b79076b42f3b1d69ba5bab2922e3
Larkrak <larkrak010@gmail.com>
2021-01-27 19:36:54 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormPublishingComponent } from './form-publishing/form-publishing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormPublishingComponent
   ],
   imports: [
     BrowserModule,
