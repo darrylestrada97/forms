6921c1408cd376ecc37292c4a1afe20f6d2bfb7a
Luis Corrales <luisgcobra@gmail.com>
2021-01-27 19:47:28 +0100

    form echo mas o menos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,12 @@ import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { FormPublishingComponent } from './form-publishing/form-publishing.component';
-=======
-import { FormCountryComponent } from './form-country/form-country.component';
->>>>>>> 83e29d0856254874b9ac5917e6f2a574cae58f89
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     FormPublishingComponent
-=======
-    FormCountryComponent
->>>>>>> 83e29d0856254874b9ac5917e6f2a574cae58f89
   ],
   imports: [
     BrowserModule,
