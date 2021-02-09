14cd6c6cf0cc9f548d8c4dab13471f9006ff0532
Larkrak <larkrak010@gmail.com>
2021-02-09 19:53:49 +0100

    .

--- src/app/form-publishing/form-publishing.component.ts
+++ src/app/form-publishing/form-publishing.component.ts
@@ -22,7 +22,6 @@ export class FormPublishingComponent implements OnInit {
 
   fields = ['Biology', 'Physics', 'Biotechnology', 'Genomics', 'Pharmaceutical', "Life"]
 
-
   JournalsObj = [];  
 
   ngOnInit(): void {
@@ -30,11 +29,30 @@ export class FormPublishingComponent implements OnInit {
   }
 
   submitted = false;
+  
 
   onSubmit(){
     this.submitted = true;
     
     console.log(this.objSearch);
   }
+
+  textInput: String;
+
+
+  onChange($event){
+    this.textInput = $event.srcElement.value;
+    
+    var inputArr = this.textInput.split(" ").filter(function(entry) { 
+                                                      return entry.trim() != ''; 
+                                                    });
+
+    console.log(inputArr.map(function(entry){
+      return entry.trim().split("")
+    }).filter(function(entry){
+      return entry ? "" : null;
+    }))
+  }
+
   
 }
