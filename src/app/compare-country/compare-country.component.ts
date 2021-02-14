4e3d3a30ba7bfb7e3b198ff9a63730f273359011
darryl <darrylestrada97@gmail.com>
2021-02-14 12:10:48 +0100

    Adding country to form

--- src/app/compare-country/compare-country.component.ts
+++ src/app/compare-country/compare-country.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Country } from '@angular-material-extensions/select-country';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 
@@ -18,32 +19,34 @@ export class CompareCountryComponent implements OnInit {
   countryFormControl = new FormControl();
   countryFormGroup: FormGroup;
 
-  private buildForm(){
-  
+  private buildForm() {
+
   }
   constructor(private formBuilder: FormBuilder) {
-    
-   }
+
+  }
 
   ngOnInit(): void {
-    
+
 
     this.countryFormGroup = this.formBuilder.group({
       country: []
     });
-    
-    this.countryFormGroup.get('country').valueChanges
-.subscribe(country => console
-.log('this.countryFormGroup.get("country").valueChanges', country));
 
-    this.countryFormControl.valueChanges
-.subscribe(country => console
-.log('this.countryFormControl.valueChanges', country));
+    // this.countryFormGroup.get('country').valueChanges
+    //   .subscribe(country => console
+    //     .log( country));
+
+    // this.countryFormControl.valueChanges.subscribe(country =>
+    //    console.log(country));
   }
 
 
-  onCountrySelected($event) {
+  onCountrySelected($event: Country) {
     console.log($event);
   }
+  onCountrySelected2($event: Country) {
+    console.log($event);
   }
+}
 
